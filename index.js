b8fbae93d0ebe8669dd4acbcd37c8055341a26c5
alshahabrezvi <alshahabrezvi200@gmail.com>
2024-02-08 21:40:00 +0600

    done country api

--- index.js
+++ index.js
@@ -1,36 +1,29 @@
 const express = require("express");
 const geoip = require("geoip-lite2");
 const app = express();
-var cors = require('cors')
+var cors = require("cors");
 const port = 3001;
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 app.set("trust proxy", true);
 app.post("/", (req, res) => {
-  const blockedCountries = ["China", "North Korea"];
   // const ip = "37.111.247.249";
   const ip = req.body.ip;
-
-  console.log(ip)
-  
+  console.log(ip);
   const geo = geoip.lookup(ip);
   console.log(geo);
   if (geo) {
-    if (blockedCountries.includes(geo.country)) {
-      res.send("Not avelable in your country.");
-    } else {
-      res.send(
-        {msg:`Hello World! Your IP address is ${ip} and your country is ${geo.country}.`,geo}
-      );
-    }
+    res
+      .send({ message: `your country api working`, country: geo.country })
+      .status(200);
   } else {
-    res.send("Hello World! Unable to get your IP address and country.");
+    res
+      .send({
+        message: `Hello World! Unable to get your IP address and country.`,
+      })
+      .status(401);
   }
 });
-
-
-
-
 
 // res.send("hi");
 
